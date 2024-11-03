e2fde419adfc75c424e603b28f16116764a596c2
tamara-danilova2105 <sap1028@mail.ru>
2024-11-03 17:39:19 +0400

    ref running line

--- src/entities/RunningLine/ui/RunningLine.tsx
+++ src/entities/RunningLine/ui/RunningLine.tsx
@@ -2,34 +2,40 @@ import { Stack } from "@/shared/ui/Stack";
 import { Text } from "@/shared/ui/Text";
 import styles from './RunningLine.module.scss';
 import logo from '@/shared/assets/png/logo_line.png'
+import React from "react";
 
 export const RunningLine = () => {
+    
+    const data = [
+        {
+            image: logo,
+            text: 'РАНЕЕ БРОНИРОВАНИЕ - 8%'
+        },
+        {
+            image: logo,
+            text: 'ПОДПИСКА НА НОВОСТИ - 3%'
+        },
+    ];
+
     return (
-        <Stack className={styles.runningLine}
-        justify="center">
-            <Stack className={styles.runningText}
+        <Stack 
+            className={styles.runningLine}
             justify="center"
-            align="center"
-            max>
-                <img src={logo} alt=""/>
-                <Text font='unbounded' color="peach" size="24">РАНЕЕ БРОНИРОВАНИЕ - 8%</Text>
-                <img src={logo} alt=""/>
-                <Text font='unbounded' color="peach" size="24">ПОДПИСКА НА НОВОСТИ - 3%</Text>
-                <img src={logo} alt=""/>
-                <Text font='unbounded' color="peach" size="24">РАНЕЕ БРОНИРОВАНИЕ - 8%</Text>
-                <img src={logo} alt=""/>
-                <Text font='unbounded' color="peach" size="24">ПОДПИСКА НА НОВОСТИ - 3%</Text>
-                <img src={logo} alt=""/>
-                <Text font='unbounded' color="peach" size="24">РАНЕЕ БРОНИРОВАНИЕ - 8%</Text>
-                <img src={logo} alt=""/>
-                <Text font='unbounded' color="peach" size="24">ПОДПИСКА НА НОВОСТИ - 3%</Text>
-                <img src={logo} alt=""/>
-                <Text font='unbounded' color="peach" size="24">ПОДПИСКА НА НОВОСТИ - 3%</Text>
-                <img src={logo} alt=""/>
-                <Text font='unbounded' color="peach" size="24">РАНЕЕ БРОНИРОВАНИЕ - 8%</Text>
-                <img src={logo} alt=""/>
-                <Text font='unbounded' color="peach" size="24">ПОДПИСКА НА НОВОСТИ - 3%</Text>
-                <img src={logo} alt=""/>
+        >
+            <Stack 
+                className={styles.runningText}
+                justify="center"
+                align="center"
+                max
+            >
+                {Array(4).fill(data).flat().map((item, index) => (
+                    <React.Fragment key={index}>
+                        <img src={item.image} alt=""/>
+                        <Text font='geometria500' color="peach" size="24">
+                            {item.text}
+                        </Text>
+                    </React.Fragment>
+                ))}
             </Stack>
         </Stack>
     );
