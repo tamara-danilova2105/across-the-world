9424c62151c771f1c5f393e601d651e81c7c2209
Богдан Ишмаев <bodyaev@MacBook-Pro-Bogdan.local>
2024-11-15 20:55:11 +0300

    upd swiper

--- src/shared/hooks/useSwipper.ts
+++ src/shared/hooks/useSwipper.ts
@@ -1,3 +1,58 @@
+// import { useState } from 'react';
+
+// interface UseSwiperProps {
+//     slidesCount: number;
+// }
+
+// export const useSwiper = ({ slidesCount }: UseSwiperProps) => {
+
+//     const [currentIndex, setCurrentIndex] = useState(0);
+//     const [startX, setStartX] = useState(0);
+//     const [startY, setStartY] = useState(0);
+//     const [isSwiping, setIsSwiping] = useState(false);
+
+//     const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+//         setStartX(e.touches[0].clientX);
+//         setStartY(e.touches[0].clientY);
+//         setIsSwiping(false);
+//     };
+
+//     const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
+//         if (!startX) return;
+//         const currentX = e.touches[0].clientX;
+//         const currentY = e.touches[0].clientY;
+//         const diffX = startX - currentX;
+//         const diffY = startY - currentY;
+
+//         if (!isSwiping && Math.abs(diffX) > Math.abs(diffY)) {
+//             setIsSwiping(true);
+//         }
+
+//         if (isSwiping) {
+//             e.preventDefault();
+
+//             if (Math.abs(diffX) > 50) {
+//                 if (diffX > 0 && currentIndex < slidesCount - 1) {
+//                     setCurrentIndex(currentIndex + 1);
+//                 } else if (diffX < 0 && currentIndex > 0) {
+//                     setCurrentIndex(currentIndex - 1);
+//                 }
+//                 setStartX(0);
+//                 setStartY(0);
+//             }
+//         }
+//     };
+
+//     const handleTouchEnd = () => {
+//         setStartX(0);
+//         setStartY(0);
+//         setIsSwiping(false);
+//     };
+
+//     return { currentIndex, setCurrentIndex, handleTouchStart, handleTouchMove, handleTouchEnd };
+// };
+
+
 import { useState } from 'react';
 
 interface UseSwiperProps {
@@ -24,7 +79,7 @@ export const useSwiper = ({ slidesCount }: UseSwiperProps) => {
         disableScroll();
 
         e.currentTarget.style.touchAction = 'pan-x';
-        e.currentTarget.style.overscrollBehaviorY='none'
+        e.currentTarget.style.overscrollBehaviorY = 'none';
     };
 
     const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
@@ -46,9 +101,12 @@ export const useSwiper = ({ slidesCount }: UseSwiperProps) => {
 
     const handleTouchEnd = (e: React.TouchEvent<HTMLDivElement>) => {
         setStartX(0);
-        enableScroll(); 
-        e.currentTarget.style.touchAction = 'auto'; 
-        e.currentTarget.style.overscrollBehaviorY=''
+        setTimeout(() => {
+            enableScroll(); // Прокрутка возвращается через 2 секунды
+        }, 1000);
+
+        e.currentTarget.style.touchAction = 'auto';
+        e.currentTarget.style.overscrollBehaviorY = '';
         setIsAnimating(false); 
     };
 
