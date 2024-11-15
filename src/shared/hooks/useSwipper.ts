c68dad667800221d4261949d7f56934c85822e0e
Богдан Ишмаев <bodyaev@MacBook-Pro-Bogdan.local>
2024-11-15 19:31:14 +0300

    upd useSwiper

--- src/shared/hooks/useSwipper.ts
+++ src/shared/hooks/useSwipper.ts
@@ -5,47 +5,52 @@ interface UseSwiperProps {
 }
 
 export const useSwiper = ({ slidesCount }: UseSwiperProps) => {
-
     const [currentIndex, setCurrentIndex] = useState(0);
     const [startX, setStartX] = useState(0);
-    const [startY, setStartY] = useState(0);
+    const [swipeDistance, setSwipeDistance] = useState(0);
     const [isSwiping, setIsSwiping] = useState(false);
 
     const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
         setStartX(e.touches[0].clientX);
-        setStartY(e.touches[0].clientY);
+        setSwipeDistance(0);
         setIsSwiping(false);
     };
 
     const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
         if (!startX) return;
-        const currentX = e.touches[0].clientX;
-        const currentY = e.touches[0].clientY;
-        const diffX = startX - currentX;
-        const diffY = startY - currentY;
 
-        if (!isSwiping && Math.abs(diffX) > Math.abs(diffY)) {
+        const currentX = e.touches[0].clientX;
+        const diffX = currentX - startX;
+
+        // Начинаем определять свайп как горизонтальный, если движение достаточно велико
+        if (!isSwiping && Math.abs(diffX) > 10) {
             setIsSwiping(true);
         }
 
         if (isSwiping) {
-            e.preventDefault();
-
-            if (Math.abs(diffX) > 50) {
-                if (diffX > 0 && currentIndex < slidesCount - 1) {
-                    setCurrentIndex(currentIndex + 1);
-                } else if (diffX < 0 && currentIndex > 0) {
-                    setCurrentIndex(currentIndex - 1);
-                }
-                setStartX(0);
-                setStartY(0);
-            }
+            setSwipeDistance(diffX);
         }
     };
 
     const handleTouchEnd = () => {
+        if (isSwiping) {
+            const screenWidth = window.innerWidth;
+            const swipeThreshold = screenWidth / 2; // Порог для пролистывания - половина экрана
+
+            if (Math.abs(swipeDistance) > swipeThreshold) {
+                if (swipeDistance < 0 && currentIndex < slidesCount - 1) {
+                    // Свайп влево, переходим на следующий слайд
+                    setCurrentIndex(currentIndex + 1);
+                } else if (swipeDistance > 0 && currentIndex > 0) {
+                    // Свайп вправо, переходим на предыдущий слайд
+                    setCurrentIndex(currentIndex - 1);
+                }
+            }
+        }
+        
+        // Сбрасываем состояния
         setStartX(0);
-        setStartY(0);
+        setSwipeDistance(0);
         setIsSwiping(false);
     };
 
