90ef349cc4c583f2d95b70c1c24848c3085c5f21
Богдан Ишмаев <bodyaev@MacBook-Pro-Bogdan.local>
2024-11-20 23:44:46 +0300

    added declensionin timer

--- src/entities/Timer/lib/useCountDownTimer.ts
+++ src/entities/Timer/lib/useCountDownTimer.ts
@@ -9,11 +9,19 @@ interface TimeLeft {
 
 interface CountdownTimerResult {
     timeItems: { value: number | undefined; label: string }[];
-    isRunning: boolean
+    isRunning: boolean;
 }
 
-export const useCountdownTimer = (endTime: string): CountdownTimerResult => {
+const declOfNum = (number: number, titles: [string, string, string]): string => {
+    const cases = [2, 0, 1, 1, 1, 2];
+    return titles[
+        number % 100 > 4 && number % 100 < 20
+            ? 2
+            : cases[number % 10 < 5 ? number % 10 : 5]
+    ];
+};
 
+export const useCountdownTimer = (endTime: string): CountdownTimerResult => {
     const calculateTimeLeft = useCallback((): TimeLeft => {
         const difference = new Date(endTime).getTime() - new Date().getTime()
         let localTimeLeft: TimeLeft = {}
@@ -24,39 +32,57 @@ export const useCountdownTimer = (endTime: string): CountdownTimerResult => {
                 hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
                 minutes: Math.floor((difference / 1000 / 60) % 60),
                 seconds: Math.floor((difference / 1000) % 60),
-            }
+            };
         }
 
         return localTimeLeft;
-    }, [endTime])
+    }, [endTime]);
 
-    const [timeLeft, setTimeLeft] = useState<TimeLeft>(calculateTimeLeft())
-    const [isRunning, setIsRunning] = useState<boolean>(true)
+    const [timeLeft, setTimeLeft] = useState<TimeLeft>(calculateTimeLeft());
+    const [isRunning, setIsRunning] = useState<boolean>(true);
 
     useEffect(() => {
         if (!isRunning) return;
 
         const timer = setInterval(() => {
-            const newTimeLeft = calculateTimeLeft()
+            const newTimeLeft = calculateTimeLeft();
             setTimeLeft(newTimeLeft);
 
             if (Object.keys(newTimeLeft).length === 0) {
                 clearInterval(timer);
                 setIsRunning(false);
             }
-        }, 1000)
+        }, 1000);
 
-        return () => clearInterval(timer)
-    }, [calculateTimeLeft, isRunning])
+        return () => clearInterval(timer);
+    }, [calculateTimeLeft, isRunning]);
 
-    //{TODO - дописать функция для обработки склонений}
     const timeItems = [
-        { value: timeLeft.days, label: 'Дней' },
-        { value: timeLeft.hours, label: 'Часов' },
-        { value: timeLeft.minutes, label: 'Минут' },
-        { value: timeLeft.seconds, label: 'Сек' }
+        {
+            value: timeLeft.days,
+            label: timeLeft.days !== undefined
+                ? declOfNum(timeLeft.days, ['День', 'Дня', 'Дней'])
+                : '',
+        },
+        {
+            value: timeLeft.hours,
+            label: timeLeft.hours !== undefined
+                ? declOfNum(timeLeft.hours, ['Час', 'Часа', 'Часов'])
+                : '',
+        },
+        {
+            value: timeLeft.minutes,
+            label: timeLeft.minutes !== undefined
+                ? declOfNum(timeLeft.minutes, ['Минута', 'Минуты', 'Минут'])
+                : '',
+        },
+        {
+            value: timeLeft.seconds,
+            label: timeLeft.seconds !== undefined
+                ? declOfNum(timeLeft.seconds, ['Секунда', 'Секунды', 'Секунд'])
+                : '',
+        },
     ];
 
     return { timeItems, isRunning }
-}
-
+};
