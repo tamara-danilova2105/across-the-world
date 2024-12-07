c3d739953cf87bd5be913545b1655ced35956a85
tamara-danilova2105 <sap1028@mail.ru>
2024-12-07 12:33:43 +0400

    added reviews tour

---
+++ src/widgets/TourDetails/ui/AboutTour/ui/ReviewsTour/ui/ReviewCard/ReviewCard.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import { Review } from '../../../../../../model/types/types';
+import styles from './ReviewCard.module.scss';
+import { Stack } from '@/shared/ui/Stack';
+
+interface ReviewCardProps {
+    review: Review;
+}
+
+const formatDate = (dateString: string): string => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('ru-RU', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+    });
+};
+
+//TODO хук для текста
+
+export const ReviewCard = ({ review }: ReviewCardProps) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const maxLength = 200;
+
+    const shouldShowButton = review.review.length > maxLength;
+
+    const displayText = shouldShowButton && !isExpanded 
+        ? `${review.review.slice(0, maxLength)}...` 
+        : review.review;
+
+    return (
+        <Stack 
+            direction='column' gap='16'
+            className={styles.reviewCard}
+        >
+            <Stack direction='column' gap='4'>
+                <div className={styles.nameCity}>
+                    {review.name}, 
+                    {review.city && <span className={styles.city}>{review.city}</span>}
+                </div>
+                <div className={styles.date}>{formatDate(review.date)}</div>
+            </Stack>
+
+            <div className={styles.review}>
+                {displayText}
+            </div>
+
+            {shouldShowButton && (
+                <button onClick={() => setIsExpanded(!isExpanded)}>
+                    {isExpanded ? 'Свернуть' : 'Показать все'}
+                </button>
+            )}
+        </Stack>
+    );
+};
