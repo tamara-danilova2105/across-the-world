7c33cb3161017798ad117dfe5c588dfdefe9f3be
Богдан Ишмаев <bodyaev@MacBook-Pro-Bogdan.local>
2025-01-22 04:45:35 +0300

    fix

--- src/feature/Signin/api/signinApi.tsx
+++ src/feature/Signin/api/signinApi.tsx
@@ -37,7 +37,7 @@ const adminApi = api.injectEndpoints({
                     url: `${endpoints.admin.login}`,
                     body,
                 }),
-            providesTags:() => ADMIN_TAG,
+            providesTags: ADMIN_TAG,
         }),
         resetPassword: build.mutation({
             query: (body: ResetPasswordBody) =>
