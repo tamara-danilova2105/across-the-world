aeea44c5d8e8cff8a8bf674778da58f342af5f7e
tamara-danilova2105 <sap1028@mail.ru>
2025-01-28 21:14:13 +0400

    added review page

--- src/feature/AddNewReview/ui/AddNewReview.tsx
+++ src/feature/AddNewReview/ui/AddNewReview.tsx
@@ -1,8 +1,29 @@
 import { Stack } from "@/shared/ui/Stack";
 import styles from './AddNewReview.module.scss';
 import { Text } from "@/shared/ui/Text";
+import { Input } from "@/shared/ui/Input";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { data } from "@/shared/lib/validateInput";
+import { Review } from "@/widgets/TourDetails/model/types/types";
+import { Button } from "@/shared/ui/Button";
+
+// _id: string;
+// name: string;
+// date: string;
+// feedback: string;
+// city?: string;
 
 export const AddNewReview = () => {
+    const onSubmit: SubmitHandler<Omit<Review, '_id'>> = (data) => {
+        console.log(data);
+    };
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Review>();
+
     return (
         <Stack
             direction="column"
@@ -16,6 +37,36 @@ export const AddNewReview = () => {
             <Text size="16">
                 Поделитесь своими впечатлениями о путешествии
             </Text>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Input
+                    label="Имя"
+                    placeholder='введите имя'
+                    register={register("name", { required: data.required })}
+                    error={errors?.name}
+                />
+
+                <Input
+                    label="Город"
+                    placeholder='введите город'
+                />
+
+                {/* TODO - выпадающий список */}
+                <Input
+                    label="Направление"
+                    placeholder='выберите направление'
+                />
+
+                {/* TODO - Label + стиль */}
+                <textarea
+                    placeholder='расскажите о вашем опыте путешествия...'
+                    rows={8}
+                />
+
+                <Button>
+                    Отправить отзыв
+                </Button>
+            </form>
         </Stack>
     );
 };
