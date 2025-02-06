9d7fe516fc29e8391fe083bc6d145d497e857ff7
tamara-danilova2105 <sap1028@mail.ru>
2025-02-06 20:45:18 +0400

    added apiTour

---
+++ src/entities/Tours/api/api.ts
@@ -0,0 +1,52 @@
+import { api } from "@/shared/api/api";
+import { endpoints } from "@/shared/api/endpoints";
+import { createApiConfig } from "@/shared/api/helper";
+
+interface TourTag {
+    type: "Tour";
+    id: string;
+}
+
+const TOUR_TAG: TourTag[] = [{ type: "Tour", id: "LIST" }];
+
+const url = endpoints.path.tours;
+
+const toursApi = api.injectEndpoints({
+    endpoints: (build) => ({
+        getAllTours: build.query({
+            query: (params) => {
+                const searchParams = new URLSearchParams(params);
+                return createApiConfig("GET", `${url}?${searchParams}`);
+            },
+            providesTags: () => TOUR_TAG,
+        }),
+        getTourById: build.query({
+            query: (id: string) => createApiConfig("GET", `${url}/${id}`),
+        }),
+        addTour: build.mutation({
+            query: (newTour) => createApiConfig("POST", `${url}`, true, newTour),
+            invalidatesTags: TOUR_TAG,
+        }),
+        editTour: build.mutation({
+            query: ({ id, updatedData }) => createApiConfig("PUT", `${url}/${id}`, true, updatedData),
+            invalidatesTags: TOUR_TAG,
+        }),
+        updateTourDetails: build.mutation({
+            query: ({ id, updateData }) => createApiConfig("PATCH", `${url}/${id}`, true, updateData),
+            invalidatesTags: TOUR_TAG,
+        }),
+        deleteTour: build.mutation({
+            query: (id: string) => createApiConfig("DELETE", `${url}/${id}`, true),
+            invalidatesTags: TOUR_TAG,
+        }),
+    }),
+});
+
+export const {
+    useGetAllToursQuery,
+    useGetTourByIdQuery,
+    useAddTourMutation,
+    useEditTourMutation,
+    useUpdateTourDetailsMutation,
+    useDeleteTourMutation,
+} = toursApi;
